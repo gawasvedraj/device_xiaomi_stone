54b95dbf10f982644215d64647315207a2d78996
Vedraj Gawas <gawasvedraj@gmail.com>
2024-09-06 10:27:59 +0530

    libinit: switch to CN props

--- libinit/init_stone.cpp
+++ libinit/init_stone.cpp
@@ -11,7 +11,7 @@
 #include "vendor_init.h"
 
 #define FINGERPRINT_GL "POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.8.0.UMPMIXM:user/release-keys"
-#define FINGERPRINT_CN "Redmi/sunstone_global/sunstone:14/UKQ1.230917.001/V816.0.5.0.UMQMIXM:user/release-keys"
+#define FINGERPRINT_CN "Redmi/sunstone/sunstone:14/UKQ1.230917.001/V816.0.3.0.UMQCNXM:user/release-keys"
 
 static const variant_info_t stone_global_info = {
     .hwc_value = "",
