e91c8eb299c507a1409131fec54829be5208ab9d
Vedraj Gawas <gawasvedraj@gmail.com>
2026-08-05 12:53:16 +0530

    CiRCLE: Bringup

--- circle_stone.mk
+++ circle_stone.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CiRCLE stuff.
+$(call inherit-product, vendor/circle/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -20,11 +20,15 @@ TARGET_SCREEN_WIDTH := 1080
 
 TARGET_EXCLUDES_AUDIOFX := true
 
+CIRCLE_BUILDTYPE := UNOFFICIAL
+CIRCLE_MAINTAINER := Vedraj
+WITH_GMS := true
+
 PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 5G
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := circle_stone
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
